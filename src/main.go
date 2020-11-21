16d4682583a0b2d5ca343dd1def21e269a33dea2
Sinem Elif Haseki <hasekisinemelif@gmail.com>
2020-11-21 05:04:12 +0300

    added distributeFixture and playOneWeek methods

--- src/main.go
+++ src/main.go
@@ -42,6 +42,29 @@ func migrate(db *sql.DB) {
 		league_name VARCHAR NOT NULL,
 		updated VARCHAR NOT NULL,
 		actions VARCHAR
+	);
+	
+	CREATE TABLE IF NOT EXISTS scores(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		team_id INTEGER NOT NULL REFERENCES teams(id),
+		won INTEGER NOT NULL,
+		drawn INTEGER NOT NULL,
+		lost INTEGER NOT NULL,
+		for INTEGER NOT NULL,
+		against INTEGER NOT NULL,
+		goal_diff INTEGER NOT NULL,
+		points INTEGER NOT NULL,
+		rate INTEGER NOT NULL
+    );
+
+	CREATE TABLE IF NOT EXISTS matches(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		week INTEGER NOT NULL,
+		home_team_id INTEGER NOT NULL REFERENCES teams(id),
+		away_team_id INTEGER NOT NULL REFERENCES teams(id),
+		home_team_score INTEGER NOT NULL,
+		away_team_score INTEGER NOT NULL,
+		is_played INTEGER NOT NULL
     );
     `
     _, err := db.Exec(sql)
@@ -64,8 +87,13 @@ func main() {
 	e.GET("/leagues/:league_id/teams/:team_id", handler.GetTeam(db))
 	e.PUT("/addleague", handler.PutLeague(db))
 	e.PUT("/leagues/:league_id/add_team", handler.PutTeam(db))
-    e.DELETE("/leagues/:league_id/delete", handler.DeleteLeague(db))
-    e.DELETE("/leagues/:league_id/delete_team/:team_id", handler.DeleteTeam(db))
+    e.DELETE("/leagues/:league_id", handler.DeleteLeague(db))
+	e.DELETE("/leagues/:league_id/teams/:team_id", handler.DeleteTeam(db))
+	// MATCH 
+	e.POST("leagues/${leagueId}/distribute-fixture", handler.DistributeFixture(db))
+	e.POST("leagues/${leagueId}/play-one-week", handler.PlayOneWeek(db))
+	e.POST("leagues/${leagueId}/play-all", handler.PlayAll(db))
+	e.POST("leagues/${leagueId}/predict-leaders", handler.PredictLeaders(db))
 	
 
     // Start as a web server
--- src/models/models.go
+++ src/models/models.go
@@ -43,6 +43,7 @@ type Score struct {
 
 type Match struct {
 	ID              int     `json:"id"`
+	Week            int     `json:"week"`
 	HomeTeamID      int     `json:"home_team_id"`
 	AwayTeamID      int     `json:"away_team_id"`
 	HomeTeamScore   int     `json:"home_team_score"`
@@ -50,11 +51,11 @@ type Match struct {
 	IsPlayed        int     `json:"is_played"` // 0 if not played, else 1
 }
 
-type MatchesCollection struct {
+type MatchCollection struct {
     Matches []Match `json:"items"`
 }
 
-type ScoresCollection struct {
+type ScoreCollection struct {
     Scores []Score `json:"items"`
 }
 
@@ -213,3 +214,98 @@ func DeleteTeam(db *sql.DB, league_id int, team_id int) (int64, error) {
 
     return result.RowsAffected()
 }
+
+/*         MATCH          */
+func CreateMatch(team1 Team, team2 Team, week int) (int64, error) {
+	sql := "INSERT INTO matches(week, home_team_id, away_team_id, home_team_score, away_team_score, is_played) VALUES(?, ?, ?, ?, ?, ?)"
+
+    // Create a prepared SQL statement
+    stmt, err := db.Prepare(sql)
+    // Exit if we get an error
+    if err != nil {
+        panic(err)
+    }
+    // Make sure to cleanup after the program exits
+    defer stmt.Close()
+
+    // Replace the '?' in our prepared statement with 'values'
+    result, err2 := stmt.Exec(week, team1.ID, team2.ID, 0, 0, 0)
+    // Exit if we get an error
+    if err2 != nil {
+        panic(err2)
+    }
+
+    return result.LastInsertId()
+}
+
+func DistributeFixture(db *sql.DB, league_id int){ // I configured the league fixed as 6 weeks since there are 4 teams in leagues. However it can be changed due to the expected outcome
+	//rand.Seed(time.Now().UnixNano()) teams or weeks can be determined randomly,
+	//for the sake of simplicity it's kind of hardcoded right now
+	min := 0
+	max := 3
+	Teams := GetTeams(db, league_id)
+	id1, _ := CreateMatch(1, Teams.Teams[3], Teams.Teams[0])
+	id2, _ := CreateMatch(1, Teams.Teams[1], Teams.Teams[0])
+	id3, _ := CreateMatch(2, Teams.Teams[2], Teams.Teams[3])
+	id4, _ := CreateMatch(2, Teams.Teams[1], Teams.Teams[2])
+	id5, _ := CreateMatch(3, Teams.Teams[0], Teams.Teams[3])
+	id6, _ := CreateMatch(3, Teams.Teams[2], Teams.Teams[1])
+	id7, _ := CreateMatch(4, Teams.Teams[3], Teams.Teams[0])
+	id8, _ := CreateMatch(4, Teams.Teams[0], Teams.Teams[1])
+	id9, _ := CreateMatch(5, Teams.Teams[1], Teams.Teams[3])
+	id10, _ := CreateMatch(5, Teams.Teams[0], Teams.Teams[2])
+	id11, _ := CreateMatch(6, Teams.Teams[2], Teams.Teams[0])
+	id12, _ := CreateMatch(6, Teams.Teams[3], Teams.Teams[2])
+	
+}
+
+func PlayOneWeek(db *sql.DB, week int){
+	sql := "SELECT * FROM matches WHERE week = " + + strconv.Itoa(week)
+	rows, err := db.Query(sql)
+    // Exit if the SQL doesn't work for some reason
+    if err != nil {
+        panic(err)
+    }
+    // make sure to cleanup when the program exits
+	defer rows.Close()
+
+    result := MatchCollection{}
+    for rows.Next() {
+        match := Match{}
+		err2 := rows.Scan(&match.ID, &match.Week, &match.HomeTeamID, &match.AwayTeamID, &match.HomeTeamScore, &match.AwayTeamScore, &match.IsPlayed)
+		rand.Seed(time.Now().UnixNano()) // use random seed to determine goals
+		goal1 = rand.Intn(4) 
+		goal2 = rand.Intn(5) 
+		// update matches
+		sql = "UPDATE matches SET is_played = 1, home_team_score = ?, away_team_score = ? WHERE home_team_id = "+match.HomeTeamID +" AND away_team_id = "+match.AwayTeamID
+		stmt, err3 := db.Prepare(sql)
+		if err != nil {
+			panic(err)
+		}
+		defer stmt.Close()
+		_, err4 := stmt.Exec(goal1, goal2)
+		// new scores
+		switch {
+		case goal1 > goal2:
+			sql1 := "UPDATE scores SET won = won + 1, points = points + 3, for = for + "+goal1+", against = against + "+goal2+" WHERE team_id = "+home_team_id
+			sql2 := "UPDATE scores SET lost = lost + 1, for = for + "+goal2+", against = against + "+goal1+"WHERE team_id = "+away_team_id
+		case goal2 > goal1:
+			sql1 := "UPDATE scores SET won = won + 1, points = points + 3, for = for + "+goal2+", against = against + "+goal1+"WHERE team_id = "+away_team_id
+			sql2 := "UPDATE scores SET lost = lost + 1, for = for + "+goal1+", against = against + "+goal2+"WHERE team_id = "+home_team_id
+		case goal2 == goal1:
+			sql1 := "UPDATE scores SET drawn = drawn + 1, points = points + 1, for = for + "+goal2+", against = against + "+goal1+"WHERE team_id = "+away_team_id
+			sql2 := "UPDATE scores SET drawn = drawn + 1, points = points + 1, for = for + "+goal1+", against = against + "+goal2+"WHERE team_id = "+home_team_id
+		}
+		sql1.Query()
+		sql2.Query()
+
+		// Exit if we get an error
+        if err2 != nil {
+            panic(err2)
+		}
+}
+        result.Teams = append(result.Teams, team)
+    }
+}
+	PlayAll(db))
+	PredictLeaders(db))
