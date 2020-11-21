2d35b1d712ec5600512aa17efab041548900fa80
Sinem Elif Haseki <hasekisinemelif@gmail.com>
2020-11-21 05:24:07 +0300

    fixed errors in match methods and removed unnecessary lines

--- src/models/models.go
+++ src/models/models.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"strconv"
 	_ "github.com/mattn/go-sqlite3"
+	"math/rand"
+	"time"
 )
 
 type League struct  {
@@ -216,7 +218,7 @@ func DeleteTeam(db *sql.DB, league_id int, team_id int) (int64, error) {
 }
 
 /*         MATCH          */
-func CreateMatch(team1 Team, team2 Team, week int) (int64, error) {
+func CreateMatch(db *sql.DB, week int,team1 Team, team2 Team) (int64, error) {
 	sql := "INSERT INTO matches(week, home_team_id, away_team_id, home_team_score, away_team_score, is_played) VALUES(?, ?, ?, ?, ?, ?)"
 
     // Create a prepared SQL statement
@@ -241,26 +243,24 @@ func CreateMatch(team1 Team, team2 Team, week int) (int64, error) {
 func DistributeFixture(db *sql.DB, league_id int){ // I configured the league fixed as 6 weeks since there are 4 teams in leagues. However it can be changed due to the expected outcome
 	//rand.Seed(time.Now().UnixNano()) teams or weeks can be determined randomly,
 	//for the sake of simplicity it's kind of hardcoded right now
-	min := 0
-	max := 3
 	Teams := GetTeams(db, league_id)
-	id1, _ := CreateMatch(1, Teams.Teams[3], Teams.Teams[0])
-	id2, _ := CreateMatch(1, Teams.Teams[1], Teams.Teams[0])
-	id3, _ := CreateMatch(2, Teams.Teams[2], Teams.Teams[3])
-	id4, _ := CreateMatch(2, Teams.Teams[1], Teams.Teams[2])
-	id5, _ := CreateMatch(3, Teams.Teams[0], Teams.Teams[3])
-	id6, _ := CreateMatch(3, Teams.Teams[2], Teams.Teams[1])
-	id7, _ := CreateMatch(4, Teams.Teams[3], Teams.Teams[0])
-	id8, _ := CreateMatch(4, Teams.Teams[0], Teams.Teams[1])
-	id9, _ := CreateMatch(5, Teams.Teams[1], Teams.Teams[3])
-	id10, _ := CreateMatch(5, Teams.Teams[0], Teams.Teams[2])
-	id11, _ := CreateMatch(6, Teams.Teams[2], Teams.Teams[0])
-	id12, _ := CreateMatch(6, Teams.Teams[3], Teams.Teams[2])
+	CreateMatch(db, 1, Teams.Teams[3], Teams.Teams[0])
+	CreateMatch(db, 1, Teams.Teams[1], Teams.Teams[0])
+	CreateMatch(db, 2, Teams.Teams[2], Teams.Teams[3])
+	CreateMatch(db, 2, Teams.Teams[1], Teams.Teams[2])
+	CreateMatch(db, 3, Teams.Teams[0], Teams.Teams[3])
+	CreateMatch(db, 3, Teams.Teams[2], Teams.Teams[1])
+	CreateMatch(db, 4, Teams.Teams[3], Teams.Teams[0])
+	CreateMatch(db, 4, Teams.Teams[0], Teams.Teams[1])
+	CreateMatch(db, 5, Teams.Teams[1], Teams.Teams[3])
+	CreateMatch(db, 5, Teams.Teams[0], Teams.Teams[2])
+	CreateMatch(db, 6, Teams.Teams[2], Teams.Teams[0])
+	CreateMatch(db, 6, Teams.Teams[3], Teams.Teams[2])
 	
 }
 
 func PlayOneWeek(db *sql.DB, week int){
-	sql := "SELECT * FROM matches WHERE week = " + + strconv.Itoa(week)
+	sql := "SELECT * FROM matches WHERE week = " + strconv.Itoa(week)
 	rows, err := db.Query(sql)
     // Exit if the SQL doesn't work for some reason
     if err != nil {
@@ -273,30 +273,35 @@ func PlayOneWeek(db *sql.DB, week int){
         match := Match{}
 		err2 := rows.Scan(&match.ID, &match.Week, &match.HomeTeamID, &match.AwayTeamID, &match.HomeTeamScore, &match.AwayTeamScore, &match.IsPlayed)
 		rand.Seed(time.Now().UnixNano()) // use random seed to determine goals
-		goal1 = rand.Intn(4) 
-		goal2 = rand.Intn(5) 
+		goal1 := rand.Intn(4) 
+		goal2 := rand.Intn(5) 
 		// update matches
-		sql = "UPDATE matches SET is_played = 1, home_team_score = ?, away_team_score = ? WHERE home_team_id = "+match.HomeTeamID +" AND away_team_id = "+match.AwayTeamID
+		sql = "UPDATE matches SET is_played = 1, home_team_score = ?, away_team_score = ? WHERE home_team_id = "+strconv.Itoa(match.HomeTeamID) +" AND away_team_id = "+strconv.Itoa(match.AwayTeamID)
 		stmt, err3 := db.Prepare(sql)
-		if err != nil {
+		if err3 != nil {
 			panic(err)
 		}
 		defer stmt.Close()
-		_, err4 := stmt.Exec(goal1, goal2)
+		stmt.Exec(goal1, goal2)
 		// new scores
 		switch {
 		case goal1 > goal2:
-			sql1 := "UPDATE scores SET won = won + 1, points = points + 3, for = for + "+goal1+", against = against + "+goal2+" WHERE team_id = "+home_team_id
-			sql2 := "UPDATE scores SET lost = lost + 1, for = for + "+goal2+", against = against + "+goal1+"WHERE team_id = "+away_team_id
+			sql1 := "UPDATE scores SET won = won + 1, points = points + 3, for = for + "+strconv.Itoa(goal1) +", against = against + "+strconv.Itoa(goal2)+" WHERE team_id = "+strconv.Itoa(match.HomeTeamID)
+			sql2 := "UPDATE scores SET lost = lost + 1, for = for + "+strconv.Itoa(goal2)+", against = against + "+strconv.Itoa(goal1)+"WHERE team_id = "+strconv.Itoa(match.AwayTeamID)
+			db.Query(sql1)
+			db.Query(sql2)
 		case goal2 > goal1:
-			sql1 := "UPDATE scores SET won = won + 1, points = points + 3, for = for + "+goal2+", against = against + "+goal1+"WHERE team_id = "+away_team_id
-			sql2 := "UPDATE scores SET lost = lost + 1, for = for + "+goal1+", against = against + "+goal2+"WHERE team_id = "+home_team_id
+			sql1 := "UPDATE scores SET won = won + 1, points = points + 3, for = for + "+strconv.Itoa(goal2)+", against = against + "+strconv.Itoa(goal1)+"WHERE team_id = "+strconv.Itoa(match.AwayTeamID)
+			sql2 := "UPDATE scores SET lost = lost + 1, for = for + "+strconv.Itoa(goal1)+", against = against + "+strconv.Itoa(goal2)+"WHERE team_id = "+strconv.Itoa(match.HomeTeamID)
+			db.Query(sql1)
+			db.Query(sql2)
 		case goal2 == goal1:
-			sql1 := "UPDATE scores SET drawn = drawn + 1, points = points + 1, for = for + "+goal2+", against = against + "+goal1+"WHERE team_id = "+away_team_id
-			sql2 := "UPDATE scores SET drawn = drawn + 1, points = points + 1, for = for + "+goal1+", against = against + "+goal2+"WHERE team_id = "+home_team_id
+			sql1 := "UPDATE scores SET drawn = drawn + 1, points = points + 1, for = for + "+strconv.Itoa(goal2)+", against = against + "+strconv.Itoa(goal1)+"WHERE team_id = "+strconv.Itoa(match.AwayTeamID)
+			sql2 := "UPDATE scores SET drawn = drawn + 1, points = points + 1, for = for + "+strconv.Itoa(goal1)+", against = against + "+strconv.Itoa(goal2)+"WHERE team_id = "+strconv.Itoa(match.HomeTeamID)
+			db.Query(sql1)
+			db.Query(sql2)
 		}
-		sql1.Query()
-		sql2.Query()
+		
 
 		// Exit if we get an error
         if err2 != nil {
